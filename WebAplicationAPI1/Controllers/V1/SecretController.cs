7b907fbbcc87a6438ebd2e3b5e8b567e681eb674
Nguyễn Thế Tình <37909676+NguyenTheTinh-HCMUS@users.noreply.github.com>
2020-09-14 22:07:46 +0700

    Creating Api Key

---
+++ WebAplicationAPI1/Controllers/V1/SecretController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAplicationAPI1.Filters;
+
+namespace WebAplicationAPI1.Controllers.V1
+{
+    [ApiKeyAuth]
+    public class SecretController: ControllerBase
+    {
+        [HttpGet("secret")]
+        public IActionResult Getsecret()
+        {
+            return Ok("I have no secrets");
+        }
+    }
+}
